4e9e52b50f2ff5a2253f0c54061c4a38cedf08a7
thatton <thomasrichardhatton@googlemail.com>
2024-07-03 11:10:34 +0100

    Added Razor Client
    - Blazor CLient is SPA so deploys as a buch of static javascript files thhus requires nginx in Docker doable but complicated
    - Simple Razor Pages app that displays Notifications from API
    - Requires CORS changes not committed yet

---
+++ Notifications.RazorClient/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+"use strict";
+//environment.baseUrl+
+var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:5026/notifications").build();
+
+
+connection.on("ReceiveNotification", function (message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${message}`;
+});
+
+connection.start().then(function () {
+    //document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
